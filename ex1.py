1f15cd7a6a4d009326b5fa4eea3aded32da6e69c
zaqueuha <maria10052008graziane@gmail.com>
2023-10-24 11:03:32 -0300

    exemplos

---
+++ ex1.py
@@ -0,0 +1,6 @@
+n=int(input("digite um numero"))
+r=n%2
+if r==0:
+     print("o valor que voce digitou e par")
+else:
+     print("o numero que voce digitou e impar")
---
+++ ex2.py
@@ -0,0 +1,6 @@
+d=float(input('qual a distancia da viagem em quilometros'))
+if d<=200:
+    print('voce pagara',d*0.50)
+else:
+    print('voce pagara',d*0.45)
+    
